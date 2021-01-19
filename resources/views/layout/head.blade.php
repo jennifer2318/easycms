132ca14ea3737ee204f512181e43d7812d71b307
pguczol23 <38167698+pguczol23@users.noreply.github.com>
2021-01-19 16:39:29 +0300

    Initial commit

---
+++ resources/views/layout/head.blade.php
@@ -0,0 +1,17 @@
+<?php
+    $user = \Illuminate\Support\Facades\Auth::user();
+?>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>EasyCms</title>
+
+    <script>
+
+    </script>
+
+</head>
+<body>
