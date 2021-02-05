59f2b71ed9963ef60d1e41565946bfa83b7ea39f
jennifer23 <38167698+jennifer23@users.noreply.github.com>
2021-02-05 04:50:12 +0300

    #update default templates

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,2 +1,3 @@
+<?php get_footer(); ?>
 </body>
 </html>
--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -4,14 +4,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>EasyCms</title>
-
-    <script>
-
-    </script>
-
+    <?php get_header(); ?>
 </head>
 <body>
